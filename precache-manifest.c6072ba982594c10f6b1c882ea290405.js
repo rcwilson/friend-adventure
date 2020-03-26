e5339561401b490a8479052f3ff110f1e2cd72a6
richard wilson <rcwilson88@gmail.com>
2020-03-26 18:10:57 -0400

    Updates

---
+++ precache-manifest.c6072ba982594c10f6b1c882ea290405.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6283be8c2d1a3e31f55957b7db4b6dd",
+    "url": "/friend-adventure/index.html"
+  },
+  {
+    "revision": "95f8e11f29dea4692cf1",
+    "url": "/friend-adventure/static/css/main.63e53389.chunk.css"
+  },
+  {
+    "revision": "94619ef4c51d9659e255",
+    "url": "/friend-adventure/static/js/2.e7eb1023.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/friend-adventure/static/js/2.e7eb1023.chunk.js.LICENSE"
+  },
+  {
+    "revision": "95f8e11f29dea4692cf1",
+    "url": "/friend-adventure/static/js/main.fb39f510.chunk.js"
+  },
+  {
+    "revision": "bb12dd9da1e585c7fc97",
+    "url": "/friend-adventure/static/js/runtime-main.f77230d8.js"
+  },
+  {
+    "revision": "d112114058ff18d508e767e7f8334395",
+    "url": "/friend-adventure/static/media/paper_fibers.d1121140.png"
+  }
+]);
--- src/components/Header.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './Header.css'
-
-export default function Header() {
-    return (
-        <header><h3>Friend Adventure</h3></header>
-    )
-}
